f939ea86c1e9132e9e6d786a235a88754d6f7f53
Hyftar <simon_landry@ymail.com>
2018-04-06 01:02:26 -0400

    Initial commit

---
+++ spec/keyboard-heatmap-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KeyboardHeatmapView from '../lib/keyboard-heatmap-view';
+
+describe('KeyboardHeatmapView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
