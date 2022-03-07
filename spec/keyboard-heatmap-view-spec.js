1c27b5e78f80a7f242ba6e74d142cdf2735cf562
Hyftar <landry_simon@pm.me>
2022-03-06 22:05:46 -0500

    Fix #7 by switching the event from keydown to keyup

--- spec/keyboard-heatmap-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import KeyboardHeatmapView from '../lib/keyboard-heatmap-view';
-
-describe('KeyboardHeatmapView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
